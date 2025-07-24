4bf0487945eac72720fa21ee08c89a6203e933ec
Precious John <preciousjohnsoninyang@Preciouss-MacBook-Pro.local>
2025-07-24 20:24:12 +0200

    icon update2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { Waitlist } from "./screens/Waitlist";
 import { About } from "./screens/About";
 
 export const App = (): JSX.Element => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Waitlist />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Waitlist />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 };
