281ede499abba788d6dacb5ad474d5d4987323f1
Precious John <preciousjohnsoninyang@Preciouss-MacBook-Pro.local>
2025-07-24 20:55:42 +0200

    updated favicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Waitlist } from "./screens/Waitlist";
